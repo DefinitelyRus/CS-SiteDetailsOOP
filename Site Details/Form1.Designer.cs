f1344e7c64dd508c39101f7a41ef5ab904342bb5
Rus <definitelyrus@gmail.com>
2022-04-03 14:10:49 +0800

    Added simulation of tabs through buttons

--- Site Details/Form1.Designer.cs
+++ Site Details/Form1.Designer.cs
@@ -28,12 +28,242 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.aboutButton = new System.Windows.Forms.Button();
+            this.rulesButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.rateButton = new System.Windows.Forms.Button();
+            this.opt1 = new System.Windows.Forms.Button();
+            this.opt2 = new System.Windows.Forms.Button();
+            this.op3 = new System.Windows.Forms.Button();
+            this.op4 = new System.Windows.Forms.Button();
+            this.op5 = new System.Windows.Forms.Button();
+            this.opt1Label = new System.Windows.Forms.Label();
+            this.opt2Label = new System.Windows.Forms.Label();
+            this.opt3Label = new System.Windows.Forms.Label();
+            this.opt4Label = new System.Windows.Forms.Label();
+            this.opt5Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Font = new System.Drawing.Font("Segoe UI Black", 40F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.titleLabel.Location = new System.Drawing.Point(12, 12);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(767, 94);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Yummyteh fud da sai\'";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // aboutButton
+            // 
+            this.aboutButton.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.aboutButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.aboutButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.aboutButton.Font = new System.Drawing.Font("Segoe UI Light", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.aboutButton.Location = new System.Drawing.Point(785, 12);
+            this.aboutButton.Name = "aboutButton";
+            this.aboutButton.Size = new System.Drawing.Size(217, 94);
+            this.aboutButton.TabIndex = 2;
+            this.aboutButton.Text = "About Us";
+            this.aboutButton.UseVisualStyleBackColor = false;
+            // 
+            // rulesButton
+            // 
+            this.rulesButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.rulesButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.rulesButton.Font = new System.Drawing.Font("Segoe UI Light", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rulesButton.Location = new System.Drawing.Point(1008, 12);
+            this.rulesButton.Name = "rulesButton";
+            this.rulesButton.Size = new System.Drawing.Size(207, 94);
+            this.rulesButton.TabIndex = 3;
+            this.rulesButton.Text = "Rules";
+            this.rulesButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.Location = new System.Drawing.Point(12, 112);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(1426, 678);
+            this.textBox1.TabIndex = 4;
+            // 
+            // rateButton
+            // 
+            this.rateButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.rateButton.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.rateButton.Font = new System.Drawing.Font("Segoe UI Light", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rateButton.Location = new System.Drawing.Point(1221, 12);
+            this.rateButton.Name = "rateButton";
+            this.rateButton.Size = new System.Drawing.Size(217, 94);
+            this.rateButton.TabIndex = 5;
+            this.rateButton.Text = "Rate Us";
+            this.rateButton.UseVisualStyleBackColor = true;
+            // 
+            // opt1
+            // 
+            this.opt1.Font = new System.Drawing.Font("Segoe UI Light", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.opt1.Location = new System.Drawing.Point(198, 193);
+            this.opt1.Name = "opt1";
+            this.opt1.Size = new System.Drawing.Size(1240, 75);
+            this.opt1.TabIndex = 6;
+            this.opt1.Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
+            this.opt1.UseVisualStyleBackColor = true;
+            this.opt1.Visible = false;
+            // 
+            // opt2
+            // 
+            this.opt2.Font = new System.Drawing.Font("Segoe UI Light", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.opt2.Location = new System.Drawing.Point(198, 274);
+            this.opt2.Name = "opt2";
+            this.opt2.Size = new System.Drawing.Size(1240, 75);
+            this.opt2.TabIndex = 7;
+            this.opt2.Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
+            this.opt2.UseVisualStyleBackColor = true;
+            this.opt2.Visible = false;
+            // 
+            // op3
+            // 
+            this.op3.Font = new System.Drawing.Font("Segoe UI Light", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.op3.Location = new System.Drawing.Point(198, 355);
+            this.op3.Name = "op3";
+            this.op3.Size = new System.Drawing.Size(1240, 75);
+            this.op3.TabIndex = 8;
+            this.op3.Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
+            this.op3.UseVisualStyleBackColor = true;
+            this.op3.Visible = false;
+            // 
+            // op4
+            // 
+            this.op4.Font = new System.Drawing.Font("Segoe UI Light", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.op4.Location = new System.Drawing.Point(198, 436);
+            this.op4.Name = "op4";
+            this.op4.Size = new System.Drawing.Size(1240, 75);
+            this.op4.TabIndex = 9;
+            this.op4.Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
+            this.op4.UseVisualStyleBackColor = true;
+            this.op4.Visible = false;
+            // 
+            // op5
+            // 
+            this.op5.Font = new System.Drawing.Font("Segoe UI Light", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.op5.Location = new System.Drawing.Point(198, 517);
+            this.op5.Name = "op5";
+            this.op5.Size = new System.Drawing.Size(1240, 75);
+            this.op5.TabIndex = 10;
+            this.op5.Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit";
+            this.op5.UseVisualStyleBackColor = true;
+            this.op5.Visible = false;
+            // 
+            // opt1Label
+            // 
+            this.opt1Label.Font = new System.Drawing.Font("Segoe UI Black", 27F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.opt1Label.Location = new System.Drawing.Point(12, 193);
+            this.opt1Label.Name = "opt1Label";
+            this.opt1Label.Size = new System.Drawing.Size(180, 75);
+            this.opt1Label.TabIndex = 11;
+            this.opt1Label.Text = "5 stars";
+            this.opt1Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.opt1Label.Visible = false;
+            // 
+            // opt2Label
+            // 
+            this.opt2Label.Font = new System.Drawing.Font("Segoe UI Black", 27F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.opt2Label.Location = new System.Drawing.Point(12, 274);
+            this.opt2Label.Name = "opt2Label";
+            this.opt2Label.Size = new System.Drawing.Size(180, 75);
+            this.opt2Label.TabIndex = 12;
+            this.opt2Label.Text = "4 stars";
+            this.opt2Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.opt2Label.Visible = false;
+            // 
+            // opt3Label
+            // 
+            this.opt3Label.Font = new System.Drawing.Font("Segoe UI Black", 27F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.opt3Label.Location = new System.Drawing.Point(12, 355);
+            this.opt3Label.Name = "opt3Label";
+            this.opt3Label.Size = new System.Drawing.Size(180, 75);
+            this.opt3Label.TabIndex = 13;
+            this.opt3Label.Text = "3 stars";
+            this.opt3Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.opt3Label.Visible = false;
+            // 
+            // opt4Label
+            // 
+            this.opt4Label.Font = new System.Drawing.Font("Segoe UI Black", 27F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.opt4Label.Location = new System.Drawing.Point(12, 436);
+            this.opt4Label.Name = "opt4Label";
+            this.opt4Label.Size = new System.Drawing.Size(180, 75);
+            this.opt4Label.TabIndex = 14;
+            this.opt4Label.Text = "2 stars";
+            this.opt4Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.opt4Label.Visible = false;
+            // 
+            // opt5Label
+            // 
+            this.opt5Label.Font = new System.Drawing.Font("Segoe UI Black", 27F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.opt5Label.Location = new System.Drawing.Point(12, 517);
+            this.opt5Label.Name = "opt5Label";
+            this.opt5Label.Size = new System.Drawing.Size(180, 75);
+            this.opt5Label.TabIndex = 15;
+            this.opt5Label.Text = "1 star";
+            this.opt5Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.opt5Label.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.aboutButton;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1450, 802);
+            this.Controls.Add(this.opt5Label);
+            this.Controls.Add(this.opt4Label);
+            this.Controls.Add(this.opt3Label);
+            this.Controls.Add(this.opt2Label);
+            this.Controls.Add(this.opt1Label);
+            this.Controls.Add(this.op5);
+            this.Controls.Add(this.op4);
+            this.Controls.Add(this.op3);
+            this.Controls.Add(this.opt2);
+            this.Controls.Add(this.opt1);
+            this.Controls.Add(this.rateButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.rulesButton);
+            this.Controls.Add(this.aboutButton);
+            this.Controls.Add(this.titleLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MdiChildrenMinimizedAnchorBottom = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "yummytehfuddasaidotcomnyaaa~";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        public Label titleLabel;
+        public Button aboutButton;
+        public Button rulesButton;
+        internal TextBox textBox1;
+        public Button rateButton;
+        public Button opt1;
+        public Button opt2;
+        public Button op3;
+        public Button op4;
+        public Button op5;
+        public Label opt1Label;
+        public Label opt2Label;
+        public Label opt3Label;
+        public Label opt4Label;
+        public Label opt5Label;
     }
 }
