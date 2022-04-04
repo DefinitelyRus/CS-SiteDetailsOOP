5df5dafdb0c3bc1ae9428ad5934f762ded96af2b
Rus <definitelyrus@gmail.com>
2022-04-04 12:24:48 +0800

    Added UI events

--- Site Details/Form1.cs
+++ Site Details/Form1.cs
@@ -7,6 +7,48 @@ namespace Site_Details
             InitializeComponent();
         }
 
+        private void Form1_OnLoad(object sender, EventArgs e)
+        {
+        }
+
+        private void aboutBtn_OnClick(object sender, EventArgs e)
+        {
+        }
+
+        private void RulesBtn_OnClick(object sender, EventArgs e)
+        {
+        }
+
+        private void RateBtn_OnClick(object sender, EventArgs e)
+        {
+        }
+
+        #region Rate button events
+        private void Rate5Btn_OnClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Rate4Btn_OnClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Rate3Btn_OnClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Rate2Btn_OnClick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Rate1Btn_OnClick(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
     public class SiteDetails
     {
         public SiteDetails(string[] aboutTexts, string[] rulesTexts, string[] rateTexts)
