3651e2b4a76ec0991736b963ec9a3ab372ec416f
Rus <definitelyrus@gmail.com>
2022-04-04 12:23:22 +0800

    Added SiteDetails object

--- Site Details/Form1.cs
+++ Site Details/Form1.cs
@@ -6,5 +6,20 @@ namespace Site_Details
         {
             InitializeComponent();
         }
+
+    public class SiteDetails
+    {
+        public SiteDetails(string[] aboutTexts, string[] rulesTexts, string[] rateTexts)
+        {
+            this.About = aboutTexts;
+            this.Rules = rulesTexts;
+            this.Rate = rateTexts;
+        }
+
+        public string[] About { get; set; }
+
+        public string[] Rules { get; set; }
+
+        public string[] Rate { get; set; }
     }
 }
